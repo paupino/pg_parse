7420f07d96d501fdbbec447466dbe8ab57b43acd
Paul Mason <paul@paulmason.me>
2021-12-23 18:06:29 -0800

    Fixes typemods by parsing Value correctly

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Add the following to your `Cargo.toml`
 
 ```toml
 [dependencies]
-pg_query = "0.2"
+pg_query = "0.3"
 ```
 
 ## Example: Parsing a query
--- tests/parse_tests.rs
+++ tests/parse_tests.rs
@@ -27,7 +27,7 @@ fn it_can_generate_a_create_index_ast() {
 #[test]
 fn it_can_generate_a_create_table_ast() {
     let result =
-        pg_query::parse("CREATE TABLE contacts.person(id serial primary key, name text not null);");
+        pg_query::parse("CREATE TABLE contacts.person(id serial primary key, name text not null, balance numeric(5, 12));");
     assert!(result.is_ok());
     let result = result.unwrap();
     let el: &Node = &result[0];
@@ -41,7 +41,59 @@ fn it_can_generate_a_create_table_ast() {
             );
             assert_eq!(relation.relname, Some("person".to_string()), "relname");
             let columns = stmt.table_elts.as_ref().expect("columns");
-            assert_eq!(2, columns.len(), "Columns length");
+            assert_eq!(3, columns.len(), "Columns length");
+            let balance = &columns[2];
+            let column = match balance {
+                Node::ColumnDef(def) => def,
+                _ => panic!("Unexpected column type"),
+            };
+            assert_eq!(column.colname, Some("balance".into()));
+            let ty = match &column.type_name {
+                Some(t) => t,
+                None => panic!("Missing type for column balance"),
+            };
+
+            // Check the name of the type, and the modifiers
+            let names = match &ty.names {
+                Some(n) => n,
+                None => panic!("No type names found"),
+            };
+            assert_eq!(names.len(), 2);
+            match &names[0] {
+                Node::String { value } => assert_eq!(value, &Some("pg_catalog".into())),
+                unexpected => panic!("Unexpected type for name[0] {:?}", unexpected),
+            }
+            match &names[1] {
+                Node::String { value } => assert_eq!(value, &Some("numeric".into())),
+                unexpected => panic!("Unexpected type for name[1] {:?}", unexpected),
+            }
+
+            // Do the mods
+            let mods = match &ty.typmods {
+                Some(m) => m,
+                None => panic!("No type mods found"),
+            };
+            assert_eq!(mods.len(), 2);
+            match &mods[0] {
+                Node::A_Const(pg_query::ast::A_Const { val, .. }) => {
+                    let constant = match **val {
+                        pg_query::ast::Value(Node::Integer { value }) => value,
+                        _ => panic!("Expected value"),
+                    };
+                    assert_eq!(constant, 5);
+                }
+                unexpected => panic!("Unexpected type for mods[0] {:?}", unexpected),
+            }
+            match &mods[1] {
+                Node::A_Const(pg_query::ast::A_Const { val, .. }) => {
+                    let constant = match **val {
+                        pg_query::ast::Value(Node::Integer { value }) => value,
+                        _ => panic!("Expected value"),
+                    };
+                    assert_eq!(constant, 12);
+                }
+                unexpected => panic!("Unexpected type for mods[0] {:?}", unexpected),
+            }
         }
         _ => panic!("Unexpected type"),
     }
