f708b84578dc95777b3a6f64665c72c2a0c880ad
Paul Mason <paul@paulmason.me>
2023-07-26 21:02:54 -0700

    Switch to unsupported instead of unimplemented

--- tests/str_tests.rs
+++ tests/str_tests.rs
@@ -260,6 +260,17 @@ fn it_correctly_converts_to_string_for_delete_tests() {
 }
 
 #[test]
+fn it_correctly_converts_to_string_for_merge_tests() {
+    let tests = [
+        "MERGE INTO customer_account ca USING recent_transactions t ON t.customer_id = ca.customer_id WHEN MATCHED THEN UPDATE SET balance = balance + transaction_value WHEN NOT MATCHED THEN INSERT (customer_id, balance) VALUES (t.customer_id, t.transaction_value)",
+        "MERGE INTO customer_account ca USING (SELECT customer_id, transaction_value FROM recent_transactions) AS t ON t.customer_id = ca.customer_id WHEN MATCHED THEN UPDATE SET balance = balance + transaction_value WHEN NOT MATCHED THEN INSERT (customer_id, balance) VALUES (t.customer_id, t.transaction_value)",
+        "MERGE INTO wines w USING wine_stock_changes s ON s.winename = w.winename WHEN NOT MATCHED AND s.stock_delta > 0 THEN INSERT VALUES(s.winename, s.stock_delta) WHEN MATCHED AND w.stock + s.stock_delta > 0 THEN UPDATE SET stock = w.stock + s.stock_delta WHEN MATCHED THEN DELETE"
+    ];
+
+    execute_tests(tests)
+}
+
+#[test]
 fn it_correctly_converts_to_string_for_create_cast_tests() {
     let tests = [
         "CREATE CAST (bigint AS int4) WITH FUNCTION int4(bigint) AS ASSIGNMENT",
